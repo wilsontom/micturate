69748277724b5881c804644c8d413fc20be1fa88
wilsontom <wilsontom89@gmail.com>
2022-06-01 15:33:16 +0100

    update

--- tests/testthat/test-micturate.R
+++ tests/testthat/test-micturate.R
@@ -3,9 +3,8 @@ context("micturate")
 test_that("micturate-works", {
 
   example_file <- system.file("extdata/example_norm.csv", package = "micturate")
-  example_values <- read.csv(example_file, header = TRUE, stringsAsFactors = FALSE)
-  expect_true(is.data.frame(normVols(example_values,100)))
-  expect_error(is.data.frame(normVols(example_values,"vol")))
+  example_values <- readr::read_csv(example_file)
+
   }
 )
 
